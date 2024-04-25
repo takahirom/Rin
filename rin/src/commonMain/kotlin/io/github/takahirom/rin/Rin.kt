1bf6c9fc29337334b143531ed41df9d0a629d840
takahirom <takam.dev@gmail.com>
2024-04-25 22:30:04 +0900

    Fix iOS behavior

--- rin/src/commonMain/kotlin/io/github/takahirom/rin/Rin.kt
+++ rin/src/commonMain/kotlin/io/github/takahirom/rin/Rin.kt
@@ -1,17 +1,17 @@
 package io.github.takahirom.rin
 
 import androidx.compose.runtime.*
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.CoroutineContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 var RIN_DEBUG = false
@@ -88,7 +88,12 @@ fun <T : Any> rememberRetained(
 ): T {
     // Caution: currentCompositeKeyHash is not unique so we need to store multiple values with the same key
     val keyToUse: String = key ?: currentCompositeKeyHash.toString(36)
-    val rinViewModel: RinViewModel = viewModel(RinViewModel::class)
+    val viewModelFactory = remember {
+        viewModelFactory {
+            addInitializer(RinViewModel::class) { RinViewModel() }
+        }
+    }
+    val rinViewModel: RinViewModel = viewModel(modelClass = RinViewModel::class, factory = viewModelFactory)
     val lifecycleOwner = LocalLifecycleOwner.current
     val lifecycleOwnerHash = lifecycleOwner.hashCode().toString(36)
     val removeRetainedWhenRemovingComposition = LocalShouldRemoveRetainedWhenRemovingComposition.current
