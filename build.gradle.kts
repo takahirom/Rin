8f985797c49941d47b92e2d2ffaa3a21de69a386
takahirom <takam.dev@gmail.com>
2024-09-19 15:35:55 +0900

    Use Gradle 8.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,3 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
 }
-
-buildscript {
-    dependencies {
-        classpath(libs.compose.jb.gradle.plugin)
-    }
-}
