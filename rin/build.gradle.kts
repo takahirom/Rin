c2134503dc940957fcd88b1b07ae8a0d186b8d8b
takahirom <takam.dev@gmail.com>
2025-08-20 11:14:42 +0900

    Migrate to com.vanniktech.maven.publish plugin

--- rin/build.gradle.kts
+++ rin/build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.compose.ExperimentalComposeLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+import com.vanniktech.maven.publish.KotlinMultiplatform
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     id("org.jetbrains.compose")
-    id("module.publication")
+    id("com.vanniktech.maven.publish")
 }
 
 kotlin {
@@ -99,7 +101,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // For testing, without this, the test will fail with the error:
         // new target SDK 21 doesn't support runtime permissions but the old target SDK 24 does
-        targetSdkVersion(libs.versions.android.targetSdk.get().toInt())
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
     }
     // Wait for AGP update
 //    testOptions {
@@ -115,4 +117,37 @@ android {
 //            }
 //        }
 //    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    
+    configure(KotlinMultiplatform())
+    
+    pom {
+        name.set("Rin")
+        description.set("This library enhances Compose Multiplatform by enabling the use of `rememberRetained{}`, which is stored within ViewModel. It broadens the versatility of Compose, allowing it to be utilized in a wider array of contexts and scenarios.")
+        url.set("https://github.com/takahirom/Rin")
+        
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("takahirom")
+                name.set("takahirom")
+            }
+            developer {
+                id.set("slack")
+                name.set("Slack Technologies, LLC")
+            }
+        }
+        scm {
+            url.set("https://github.com/takahirom/Rin")
+        }
+    }
 }
--- rin/src/commonTest/kotlin/FibiTest.kt
+++ rin/src/commonTest/kotlin/FibiTest.kt
@@ -4,6 +4,8 @@ import kotlin.test.assertEquals
 class FibiTest {
 
     @Test
-    fun `test 3rd element`() {
+    fun test3rdElement() {
+        val fibonacci = listOf(0, 1, 1, 2, 3, 5, 8, 13)
+        assertEquals(1, fibonacci[2])
     }
 }
